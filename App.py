f6eea4f72fc79f78e516f7dc36174447f6601757
sebastian <sguandinangod@est.ups.edu.ec>
2020-01-25 17:46:24 -0500

    Implementacion de la ventana para el analisis de los datos

--- App.py
+++ App.py
@@ -4,6 +4,16 @@ from flask import Flask, render_template, request, redirect, url_for, session
 from flask_mysqldb import MySQL
 import MySQLdb.cursors
 import re
+from time import time
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score,confusion_matrix,roc_auc_score,roc_curve, classification_report
+from sklearn.preprocessing import StandardScaler
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
 # initializations
 app = Flask(__name__)
 
@@ -31,6 +41,7 @@ def docente():
 
 @app.route('/add_contact', methods=['POST'])
 def add_contact():
+    tiempo_inicial = time()
     ms=' '
     if request.method == 'POST':
 
@@ -67,11 +78,18 @@ def add_contact():
             cur.execute("INSERT INTO docente (id_docente, nombre_docente, cedula, sexo,username) VALUES ( null" + ",'" + nombre + "'," + cedula +",'"+sexo+ "','"+username+"')")
 
             mysql.connection.commit()
+            tiempo_final = time()
+
+            tiempo_ejecucion = tiempo_final - tiempo_inicial
+            print('TIEMPO DE EJECUCION INGRESO DOCENTE : ', tiempo_ejecucion)
+
 
             return redirect(url_for('docente'))
 
+
 @app.route('/edit/<id>', methods = ['POST', 'GET'])
 def get_contact(id):
+
     print("estoy editando")
     cur = mysql.connection.cursor()
     cur.execute('SELECT * FROM docente WHERE id_docente = '+id)
@@ -83,6 +101,9 @@ def get_contact(id):
 
 @app.route('/update/<id>', methods=['POST'])
 def update_contact(id):
+    tiempo_inicial = time()
+
+
     if request.method == 'POST':
         nombre = request.form['nombre']
         cedula = request.form['cedula']
@@ -94,19 +115,31 @@ def update_contact(id):
 
         #flash('Contact Updated Successfully')
         mysql.connection.commit()
+        tiempo_final = time()
+
+        tiempo_ejecucion = tiempo_final - tiempo_inicial
+        print('TIEMPO DE EJECUCION ACTUALIZACION DEL CONTACTO : ', tiempo_ejecucion)
         return redirect(url_for('docente'))
+
 
 @app.route('/delete/<string:id>', methods = ['POST','GET'])
 def delete_contact(id):
+    tiempo_inicial = time()
     cur = mysql.connection.cursor()
     cur.execute('DELETE FROM docente WHERE id_docente = {0}'.format(id))
     mysql.connection.commit()
+    tiempo_final = time()
+
+    tiempo_ejecucion = tiempo_final - tiempo_inicial
+    print('TIEMPO DE EJECUCION ELIMINACION DEL CONTACTO: ', tiempo_ejecucion)
 
     return redirect(url_for('docente'))
 
 
+
 @app.route('/horario')
 def horario1():
+    tiempo_inicial = time()
     cur = mysql.connection.cursor()
     cur.execute('SELECT * FROM docente')
     data = cur.fetchall()
@@ -119,13 +152,18 @@ def horario1():
     cur.execute('SELECT horario.id_horario, docente.nombre_docente,materia.nombre_materia,hora_entrada,hora_salida,dia,grupo, aulab FROM horario,docente,materia WHERE docente.id_docente=horario.id_docente AND materia.id_materia=horario.id_materia')
     horarios=cur.fetchall()
     cur.close()
-    return render_template("horario.html",contacts=data,contact=data1,au=au,lab=lab,horarios=horarios)
+    tiempo_final = time()
 
+    tiempo_ejecucion = tiempo_final - tiempo_inicial
+    print('TIEMPO DE EJECUCION CONSULAT DOCENTE, LAB,HORARIOS: ', tiempo_ejecucion)
+
+    return render_template("horario.html",contacts=data,contact=data1,au=au,lab=lab,horarios=horarios)
 
 
 
 @app.route('/bloque')
 def bloque1():
+    tiempo_inicial = time()
     cur = mysql.connection.cursor()
     cur.execute('SELECT * FROM bloques')
 
@@ -136,11 +174,17 @@ def bloque1():
     cur.execute('SELECT laboratorios.id,bloques.nombre_bloque,laboratorios.numero_lab FROM laboratorios,bloques WHERE bloques.id_bloque=laboratorios.id_bloque')
     data2 = cur.fetchall()
     cur.close()
+    tiempo_final = time()
+
+    tiempo_ejecucion = tiempo_final - tiempo_inicial
+    print('TIEMPO DE EJECUCION CONSULTA BLOQUES: ', tiempo_ejecucion)
 
     return render_template("registro_bloque.html",contacts=data,aulas=data1,laboratorios=data2)
 
+
 @app.route('/materia')
 def materia1():
+
     cur = mysql.connection.cursor()
     cur.execute('SELECT * FROM materia')
 
@@ -149,6 +193,7 @@ def materia1():
 
 @app.route('/insert_materia', methods=['POST'])
 def insert_materia():
+    tiempo_inicial = time()
     nbloque = request.form['nbloque']
     print(nbloque)
     s="INSERT INTO materia (id_materia, nombre_materia) VALUES (NULL, '"+nbloque+"');"
@@ -157,18 +202,29 @@ def insert_materia():
     cur.execute(s)
 
     mysql.connection.commit()
+    tiempo_final = time()
+
+    tiempo_ejecucion = tiempo_final - tiempo_inicial
+    print('TIEMPO DE EJECUCION INSERTAR MATERIAS: ', tiempo_ejecucion)
 
     return redirect(url_for('materia1'))
 
+
 @app.route('/deleteC/<string:idb>', methods = ['POST','GET'])
 def deleteC(idb):
+    tiempo_inicial = time()
     cur = mysql.connection.cursor()
     q='DELETE FROM materia WHERE id_materia = {0}'.format(idb)
     cur.execute(q)
     print(q)
     mysql.connection.commit()
-    return redirect(url_for('materia1'))
+    tiempo_final = time()
 
+    tiempo_ejecucion = tiempo_final - tiempo_inicial
+    print('TIEMPO DE EJECUCION ELIMAR MATERIAS: ', tiempo_ejecucion)
+
+
+    return redirect(url_for('materia1'))
 
 
 
@@ -179,6 +235,7 @@ def deleteC(idb):
 @app.route('/insert_bloque', methods=['POST'])
 
 def insert_bloque():
+    tiempo_inicial = time()
     msg = ''
     nbloque = request.form['nbloque']
     print(nbloque)
@@ -212,7 +269,12 @@ def insert_bloque():
         cur.execute(s)
 
         mysql.connection.commit()
-        return redirect(url_for('bloque1'))
+        tiempo_final = time()
+
+        tiempo_ejecucion = tiempo_final - tiempo_inicial
+        print('TIEMPO DE EJECUCION INSERTAR BLOQUES: ', tiempo_ejecucion)
+
+    return redirect(url_for('bloque1'))
 
 @app.route('/deleteB/<string:idb>', methods = ['POST','GET'])
 def deleteB(idb):
@@ -225,6 +287,7 @@ def deleteB(idb):
 
 @app.route('/insert_horario', methods=['POST'])
 def insert_horario():
+    tiempo_inicial = time()
     print("··························")
     iddocente = request.form['iddocente']
     idmateria = request.form['idmateria']
@@ -244,7 +307,13 @@ def insert_horario():
     cur.execute(query1)
     mysql.connection.commit()
 
+    tiempo_final = time()
+
+    tiempo_ejecucion = tiempo_final - tiempo_inicial
+    print('TIEMPO DE EJECUCION INSERTAR HORARIOS: ', tiempo_ejecucion)
+
     return redirect(url_for('horario1'))
+
 
 
 
@@ -252,6 +321,7 @@ def insert_horario():
 
 @app.route('/insert_bloque_aulas', methods=['POST'])
 def insert_bloque_aulas():
+    tiempo_inicial = time()
     print("··························")
     idBoque = request.form['idBloque']
     print(idBoque)
@@ -296,7 +366,14 @@ def insert_bloque_aulas():
         cur.execute(s)
         mysql.connection.commit()
         print(s)
+        tiempo_final = time()
+
+        tiempo_ejecucion = tiempo_final - tiempo_inicial
+        print('TIEMPO DE EJECUCION INSERTAR BLOQUES Y AULAS: ', tiempo_ejecucion)
+
         return redirect(url_for('bloque1'))
+
+
 # starting the app
 @app.route('/deleteL/<string:idL>', methods = ['POST','GET'])
 def deleteL(idL):
@@ -317,8 +394,10 @@ def deleteA(idA):
 
 @app.route('/pythonlogin/', methods=['GET', 'POST'])
 def login():
+
     # Output message if something goes wrong...
     msg = ''
+    tiempo_inicial = time()
     # Check if "username" and "password" POST requests exist (user submitted form)
     if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
         # Create variables for easy access
@@ -343,27 +422,42 @@ def login():
             print("estoy dentro del iff")
 
             # Create session data, we can access this data in other routes
+            tiempo_final = time()
+
+            tiempo_ejecucion = tiempo_final - tiempo_inicial
+            print('TIEMPO DE EJECUCION LOGIN: ', tiempo_ejecucion)
             return redirect(url_for('docente'))
         else:
             # Account doesnt exist or username/password incorrect
             msg = 'Incorrect username/password!'
     # Show the login form with message (if any)
+
+
     return render_template('login.html', msg=msg)
+
 
 
 # http://localhost:5000/python/logout - this will be the logout page
 @app.route('/pythonlogin/logout')
 def logout():
     # Remove session data, this will log the user out
+   tiempo_inicial = time()
    session.pop('loggedin', None)
    session.pop('id', None)
    session.pop('username', None)
    # Redirect to login page
+   tiempo_final = time()
+   tiempo_ejecucion = tiempo_final - tiempo_inicial
+   print('TIEMPO DE EJECUCION CIERRE DE SESION: ', tiempo_ejecucion)
    return redirect(url_for('login'))
+
+
+
 
 # http://localhost:5000/pythinlogin/register - this will be the registration page, we need to use both GET and POST requests
 @app.route('/pythonlogin/register', methods=['GET', 'POST'])
 def register():
+    tiempo_inicial = time()
     # Output message if something goes wrong...
     msg = ''
     # Check if "username", "password" and "email" POST requests exist (user submitted form)
@@ -402,6 +496,10 @@ def register():
             cursor.execute('INSERT INTO accounts VALUES (NULL, %s, %s, %s)', (username, password, email))
             mysql.connection.commit()
             msg = 'You have successfully registered!'
+            tiempo_final = time()
+
+            tiempo_ejecucion = tiempo_final - tiempo_inicial
+            print('TIEMPO DE EJECUCION REGISTRO DE USUARIOS: ', tiempo_ejecucion)
     elif request.method == 'POST':
         print(6)
         # Form is empty... (no POST data)
@@ -422,6 +520,7 @@ def home():
 # http://localhost:5000/pythinlogin/profile - this will be the profile page, only accessible for loggedin users
 @app.route('/pythonlogin/profile')
 def profile():
+    tiempo_inicial = time()
     # Check if user is loggedin
     if 'loggedin' in session:
         # We need all the account info for the user so we can display it on the profile page
@@ -434,7 +533,185 @@ def profile():
         account = cursor.fetchone()
         print(account)
         # Show the profile page with account info
+        tiempo_final = time()
+
+        tiempo_ejecucion = tiempo_final - tiempo_inicial
+        print('TIEMPO DE EJECUCION VISAUALIZACION D PERFILES: ', tiempo_ejecucion)
         return render_template('profile.html', accounts=account)
     # User is not loggedin redirect to login page
+
     return redirect(url_for('login'))
+
+@app.route('/machine')
+def machine():
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM docente')
+    data = cur.fetchall()
+    contact = data
+
+
+    return render_template("machine.html",contact=data)
+
+@app.route('/predecir', methods=['POST'])
+def predecir():
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM docente')
+    data = cur.fetchall()
+
+    hen = request.form['horaentrada']
+    hsa = request.form['horasalida']
+    dn = request.form['dianumero']
+    print(hen,hsa,dn)
+
+
+    reader = pd.read_csv('dataset.csv', encoding='utf8', delimiter=";")
+
+    reader = reader.drop('Nombre', axis=1)
+    # reader = reader.values
+
+    reader["hora_salida"] = reader["hora_salida"].str.split(":", n=1, expand=True)
+    reader["hora_enterada"] = reader["hora_enterada"].str.split(":", n=1, expand=True)
+    reader['hora_timbre'] = reader["hora_timbre"].str.split(":", n=1, expand=True)
+    reader['delay_time'] = reader["delay_time"].str.split(":", n=2, expand=True)
+    reader['date'] = pd.to_datetime(reader['date'])
+    date = []
+    for i in reader.date:
+        date.append(i.weekday())
+
+    reader['date'] = date
+
+    reader = reader.drop(reader.columns[[4, 6]], axis='columns')
+
+    x = reader.drop(['atraso', 'hora_timbre'], axis=1).values
+    y = reader.atraso.values
+
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+
+    escalar = StandardScaler()
+
+    # x_train = escalar.fit_transform(x_train)
+    # x_test = escalar.fit_transform(x_test)
+
+    algoritmo = LogisticRegression()
+
+    # x_train = np.reshape(1,-1)
+    # print(algoritmo)
+
+    algoritmo.fit(x_train, y_train)
+
+    H1=np.array([int(hen),int(hsa),int(dn)])
+    h1=H1.reshape(1,-1)
+    '''
+    d1 = {"hora_enterada":16,
+        "hora_salida":20,
+       "date": 3}
+
+    #df2 = pd.DataFrame(H1, index=[1])
+    '''
+    y_pred2 = algoritmo.predict(h1)
+    print(y_pred2)
+    if(y_pred2==0):
+        respredict= str(y_pred2) + " Llegara puntual"
+
+    else :
+        respredict =str(y_pred2)+ "Se atrasara por algun motivo"
+
+    y_pred = algoritmo.predict(x_test)
+
+    accuracy = accuracy_score(y_test, y_pred)
+    matrix_confusion = confusion_matrix(y_test, y_pred)
+    ################
+    print(accuracy)
+    matriz=matrix_confusion
+    PJ = []
+    for x in range(len(matriz)):
+        sumaColumna = 0
+        for y in range(len(matriz)):
+            sumaColumna = sumaColumna + matriz[y][x]
+        PJ.append(sumaColumna)
+
+    print(matriz)
+    print(PJ)
+    precision = []
+    for x1 in range(len(matriz)):
+        Pr = matriz[x1][x1] / PJ[x1]
+        print("Precision ", x1 + 1, " =", Pr)
+        precision.append(Pr)
+
+    # Recall
+    CI = []
+    for x2 in range(len(matriz)):
+        sumfilas = 0
+        for y2 in range(len(matriz)):
+            sumfilas = sumfilas + matriz[x2][y2]
+        CI.append(sumfilas)
+
+    print(CI)
+    Recall = []
+    for x3 in range(len(matriz)):
+        Re = matriz[x3][x3] / CI[x3]
+        print("Recall ", x3 + 1, " =", Re)
+        Recall.append(Re)
+
+    # Accuracy
+    sumafila = 0
+    for x4 in range(len(matriz)):
+        sumafila = sumafila + PJ[x4]
+    suma = 0
+    for x5 in range(len(matriz)):
+        suma = suma + matriz[x5][x5]
+
+    print('')
+
+    Ac = suma / sumafila
+    print("Accuracy: ", Ac)
+
+    # F1 SCORE
+    print(' ')
+    print("Precicion", precision)
+    print("Recall", Recall)
+    fscore=[]
+    for i in range(len(matriz)):
+        f1 = (2 * (precision[i] * Recall[i])) / (precision[i] + Recall[i])
+        fscore.append(f1)
+        print("F Score ", i + 1, " =", f1)
+    print("Precicion", precision)
+    print("Recall", Recall)
+    print("")
+    ##############
+    repor=classification_report(y_test, y_pred)
+    report1=repor.split('\n')
+    print(repor.split('\n'))
+
+
+    print("", classification_report(y_test, y_pred))
+    Log_roc_auc = roc_auc_score(y_test, algoritmo.predict(x_test))
+    fpr, tpr, thresholds = roc_curve(y_test, algoritmo.predict_proba(x_test)[:, 1])
+
+    plt.figure()
+
+    plt.plot(fpr, tpr, label=" Curva Roc Logistic Regression area = %0.2f" % Log_roc_auc)
+    plt.legend(loc="lower right")
+    #plt.show()
+    plt.savefig("static/image/image2.png", depi=100)
+    g2 = 'static/image/image2.png'
+##contacts=data,precision=precision,recall=Recall
+
+    return render_template ('machine.html',recall=Recall,fscore=fscore,presicion=precision, pred1=respredict,contact=data,results=repor,recomend=y_test,pred=y_pred,speech=report1,contacts=data,acc=accuracy,coseno=matrix_confusion,im2=g2)
+
+@app.route('/buscar', methods=['POST'])
+def buscar()->'html' :
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM docente')
+    data = cur.fetchall()
+
+
+    prof = request.form['profesor']
+    print(prof)
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM horario where id_docente = '+prof)
+    data1 = cur.fetchall()
+
+    return render_template('machine.html',datos=data1,contact = data)
+
 app.run()
--- analisis.py
+++ analisis.py
@@ -1,11 +1,41 @@
 import numpy as np
 import pandas as pd
+from sklearn.model_selection import KFold,LeaveOneOut, train_test_split
+from sklearn import datasets
+from sklearn import linear_model
+from sklearn.metrics import mean_squared_error
+from sklearn import model_selection
+from sklearn.metrics import r2_score
+import pandas as pd
 import matplotlib.pyplot as plt
-url="HWbyEmColorado.csv"
+import seaborn as sns
+url="aggregate.csv"
 dataset = pd.read_csv(url)
+print(dataset.head())
+
+dataset.columns
+
+print(dataset.count())
+
 print(type(dataset))
+print(dataset.head())
 print(dataset)
 m=np.matrix(dataset)
+nombre=dataset.get("Nombre")
+print("·$%&/()(&%$$%&/)(/&%")
+print(nombre)
+
+##dataset.corr(method="pearson")
+#print(dataset.corr())
+area=m[:,5]
+periodyear=m[:,6]
+periodtype=m[:,7]
+period=m[:,9]
+corrmat = dataset.corr()
+plt.plot(area,period)
+sns.heatmap(corrmat, vmax=.8, square=True);
+'''
+print("ertyuioytdfghjkl")
 #https://data.colorado.gov/Labor-Employment/Hours-Worked-by-Employees-in-Colorado/pt2g-89wc
 #stateabbrv=dataset.get('suppheallwrkr')
 st=dataset.get('suppheallwrkr')
@@ -32,10 +62,62 @@ print(X.corr())
 print(pd.DataFrame(X))
 print("################################333")
 RESUL=np.matrix
-LL=np.insert(RESUL, RESUL.shape[1], np.array(area), 1)
-n=pd.DataFrame()
-print(LL)
+print("==================================================")
 
-n.corr(method="pearson")
-print(n.corr())
 
+dataset.dropna()
+print(dataset.dropna())
+dataset.corr(method="pearson")
+print(dataset.corr())
+
+def scatterplot(x, y, x_title, y_title):
+ plt.plot(x, y, 'b.')
+ plt.xlabel(x_title)
+ plt.ylabel(y_title)
+ plt.xlim(min(x)-1, max(x)+1)
+ plt.ylim(min(y)-1, max(y)+1)
+ plt.show()
+scatterplot(area, supprecord, "DEP_TIME",
+"ARR_DELAY_NEW")
+#LL=np.insert(RESUL, RESUL.shape[1], np.array(area), 1)
+#n=pd.DataFrame()
+#print(LL)
+iris = datasets.load_iris()
+#print(iris.data[:, :4])
+#print(iris.target)
+x=iris.data[:100]
+y=iris.target[:100]
+#dataframe = pd.read_csv(r"usuarios_win_mac_lin.csv")
+#dataframe.head()
+#print(dataframe.groupby('clase').size())
+#url = 'Deber_7.csv'
+#dcsv = pd.read_csv(url)
+#print(dcsv)
+#dcsv.head()
+print("====Holdout method====")
+size=int(input("ingrese el porcentaje de para training ejemplo 70. \n"))
+#X_train,  X_test,Y_train ,Y_test =train_test_split(x, y, test_size=size)
+train=iris.data[:size,:]
+print(train)
+ytr=iris.target[0:size]
+print(ytr)
+test=iris.data[size:100,:]
+yt=iris.target[size:100]
+print("VALORES DE REALES DE Y DEL TEST ",yt)
+model = linear_model.LogisticRegression()
+model.fit(train,ytr)
+predictions = model.predict(test)
+print("VALORES DE Y ESTIMADA",predictions)
+score=model.score(test,yt)
+print("Precisión media de las predicciones",score)
+print()
+print("====K-Fold Cross-Validation====")
+k=int(input("Ingrese el numero de iteraciones  \n"))
+
+regr=linear_model.LinearRegression()
+#regr.fit(x_train,y_train)
+   # y_pred=regr.predict(x_test)
+
+#print("Error: ",mean_squared_error(y_test,y_pred)*100)
+
+'''
